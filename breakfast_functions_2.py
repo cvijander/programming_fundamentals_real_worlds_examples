2feeb4a0d4437fc2d47c725448e1bc34a4c59c35
cvijander <cvija85@gmail.com>
2023-07-23 14:54:53 +0200

    deffining a function

---
+++ breakfast_functions_2.py
@@ -0,0 +1,8 @@
+def make_an_omelette():
+    print('Mixing the ingredients')
+    print('Pouring the mixture int a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side')
+    omelette = 'a tasty omelette'
+    print (omelette)

9d466d7606175339ebed66d81c7fd28aaf3ccaa7
cvijander <57492578+cvijander@users.noreply.github.com>
2023-07-23 14:34:16 +0200

    Delete breakfast_functions_1.py

--- breakfast_functions_1.py
+++
@@ -1,9 +0,0 @@
-# A functional Breakfast
-
-print('Mixing the ingredients')
-print('Pouring the mixture int a frying pan')
-print('Cooking the first side')
-print('Flipping it!')
-print('Cooking the other side')
-omelette = 'a tasty omelette'
-print (omelette)

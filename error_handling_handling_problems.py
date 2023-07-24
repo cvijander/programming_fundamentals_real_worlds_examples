ff1beae96a1c81841602684142f2703f4c63f798
cvijander <cvija85@gmail.com>
2023-07-25 01:06:15 +0200

    error handling

---
+++ error_handling_handling_problems.py
@@ -0,0 +1,45 @@
+
+class ElectricalError(Exception):
+
+    def __init__(self,device, problem):
+        self.device = device
+        self.problem = problem
+
+    def __str__(self):
+        return "The {} is {} ".format(self.device, self.problem)
+
+
+class PlumbingError(Exception):
+
+        def __init__(self,device, problem):
+            self.device = device 
+            self.problem = problem
+
+        def __str__(self):
+            return "The {} is {} ".format(self.device,self.problem)
+
+
+def cause_error(error_type):
+
+    if error_type == 'electrical':
+         raise ElectricalError('circuit breaker', 'overload')
+    
+    elif error_type == 'plumbing':
+         raise PlumbingError('dishwasher','spraying water')
+    
+    else: 
+         raise Exception('a generic household problem')
+    
+
+
+
+try:
+      cause_error('yard')   
+except ElectricalError as e:
+     print(e)
+     print('Fixing it myself.')
+except PlumbingError as e:
+    print(e)
+    print('Call the plumber.')
+except:
+     print('Call the landlord')    

1b5e707e1d755656d4efe3800f5d3561af093b1e
cvijander <cvija85@gmail.com>
2023-07-24 23:04:39 +0200

    for loop

---
+++ loops_for_loops.py
@@ -0,0 +1,9 @@
+# dirty dishes in the sink
+
+sink = ['bowl','plate','cup']
+
+for dish in list(sink):
+    print('Putting {} in the dishwasher'.format(dish))
+    sink.remove(dish)
+
+print(sink)    

f22d437e277ecfea4642a874ed8a7168edaca59a
cvijander <cvija85@gmail.com>
2023-07-25 00:39:44 +0200

    error handling

--- error_handling_error_catching.py
+++ error_handling_error_catching.py
@@ -1,3 +1,11 @@
 import urllib.request
 
-webpage = urllib.request
+try: 
+    webpage = urllib.request.urlopen('http://www.google.com')
+
+except:
+     print('Could not access webpage!')
+
+else:   
+    for line in webpage:
+     print(line)

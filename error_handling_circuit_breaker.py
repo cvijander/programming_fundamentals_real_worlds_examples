6e14365b56a74d630f80b9de4c4ce43e6e4abbed
cvijander <cvija85@gmail.com>
2023-07-25 00:51:36 +0200

    error circut breaker exception

---
+++ error_handling_circuit_breaker.py
@@ -0,0 +1,29 @@
+class CircuitBreaker:
+
+
+    def __init__(self,max_amps):
+        self.capacity = max_amps # max capacity in amps
+        self.load = 0 # present load in amps
+
+
+    def connect(self,amps):
+        if self.load + amps > self.capacity:
+            raise Exception('connection will exceed capacity')
+        elif self.load +amps < 0:
+            raise Exception ('connection will cause negative load')
+        else:
+          self.load += amps
+
+
+# create a 20A circuit breaker
+
+cb = CircuitBreaker(20)
+
+print(cb.load)
+
+cb.connect(12)
+cb.connect(7)
+#cb.connect(10)
+#cb.connect(-40)
+
+print(cb.load)

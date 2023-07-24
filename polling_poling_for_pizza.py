7243d956549522b37843497731b9613e2a360b8e
cvijander <cvija85@gmail.com>
2023-07-25 01:42:12 +0200

    polling

---
+++ polling_poling_for_pizza.py
@@ -0,0 +1,16 @@
+
+hungry = True
+
+while(hungry):
+
+    print('Opening the front door.')
+    front_door = ['Nope','Pizza Guy']
+
+    if "Pizza Guy" in front_door:
+        print("Pizza's here!")
+        hungry = False
+    else:
+        print("Not yet...")
+
+    print('Closing the front door.')
+            

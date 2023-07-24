a665aab184b0bf3f407dfc229c0f0444ba455c9a
cvijander <cvija85@gmail.com>
2023-07-24 21:27:45 +0200

    else if

---
+++ conditional_execution_ordering_a pizza.py
@@ -0,0 +1,16 @@
+# ordering a  pizza that Verne can eat
+
+#things that Verne does not eat 
+diet_restrictions = set(['meat','cheese'])
+
+
+#decide which pizza to order
+if 'meat' and 'cheese' in diet_restrictions:
+    print('Get a vegan pizza. ')
+
+
+elif 'meat'  in diet_restrictions:
+    print('Get a cheese pizza. ')
+
+else:
+    print('Get something else.')      
